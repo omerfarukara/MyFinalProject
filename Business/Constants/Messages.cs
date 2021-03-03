c2b86ddb3995dd74b61f33e819f189bc7b3e5ed6
omerfarukara <65817612+omerfarukara@users.noreply.github.com>
2021-03-03 07:42:24 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid = "Ürün İsmi Geçersiz";
+        internal static string MaintenanceTime = "Sistem Bakımda";
+        internal static string ProductsListed = "Ürünler Listelendi";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExists = "Böyle bir isimde ürün var";
+        public static string CategoryLimitExceded = "15'ten fazla kategori var";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string UserRegistered = "Kullanıcı kayıt oldu.";
+        public static string UserNotFound = "Kullanıcı bulunamadı.";
+        public static string PasswordError = "Şifre Hatalı.";
+        public static string SuccessfulLogin = "Giriş başarılı.";
+        public static string UserAlreadyExists = "Kullanıcı Zaten giriş yaptı.";
+        public static string AccessTokenCreated = "Token oluşturuldu.";
+    }
+}
---
+++ Core/DataAccess/EntityFramwork/EfEntityRepositoryBase.cs
@@ -0,0 +1,67 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.DataAccess;
+using System.Linq.Expressions;
+using System.Linq;
+
+namespace Core.DataAccess.EntityFramwork
+{
+    public class EfEntityRepositoryBase<TEntity,TContext>:IEntityRepository<TEntity>
+        where TEntity: class, IEntity, new()
+        where TContext: DbContext,new()
+    {
+
+        public void Add(TEntity entity)
+        {
+            //IDisposable pattern implementation of c#
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity); // Referansı yakala
+                addedEntity.State = EntityState.Added; // Ekle
+                context.SaveChanges(); // Değişiklikleri kaydet
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity); // Referansı yakala
+                deletedEntity.State = EntityState.Deleted; // Sil
+                context.SaveChanges(); // Değişiklikleri kaydet
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(filter).ToList();
+
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity); // Referansı yakala
+                updatedEntity.State = EntityState.Modified; // Sil
+                context.SaveChanges(); // Değişiklikleri kaydet
+            }
+        }
+    }
+}
